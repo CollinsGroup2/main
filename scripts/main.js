5fdd237ca0215a0aded89c70b319bedc3ca577f5
Sean <102238349+sb-shu@users.noreply.github.com>
2023-02-13 12:01:14 +0000

    Add scale control, marker popups and icon

--- scripts/main.js
+++ scripts/main.js
@@ -5,23 +5,24 @@ function initLeaflet() {
     map = L.map("map");
     map.setView([54, -4], 5);
 
-    const tileLayer = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-    tileLayer.addTo(map);
     const satelliteLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
     });
     satelliteLayer.addTo(map);
+    const tileLayer = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
 
     const tileLayers = {
-        "Street": tileLayer,
-        "Satellite": satelliteLayer
+        "Satellite": satelliteLayer,
+        "Street": tileLayer
     };
 
     const layerControl = L.control.layers(tileLayers, {});
     layerControl.addTo(map);
+
+    L.control.scale().addTo(map);
 }
 
 function getPoints() {
@@ -33,7 +34,14 @@ function getPoints() {
                 const long = parseFloat(coords[i]);
                 const lat = parseFloat(coords[i + 1]);
 
-                const marker = L.marker([long, lat]);
+                const icon = L.icon({
+                    iconUrl: "assets/plane.svg",
+                    iconSize: [32, 32],
+                    iconAnchor: [16, 16]
+                });
+
+                const marker = L.marker([long, lat], {icon:icon});
+                marker.bindPopup(`This point is ${long} ${lat}`);
                 marker.addTo(map);
             }
         });
