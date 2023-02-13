e9b923b0aff80e12a7699e81b2441d2408d9f987
Sean <102238349+sb-shu@users.noreply.github.com>
2023-02-13 10:04:26 +0000

    Add initial Leaflet cde

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
-<?php require "headers.php"; ?>
 <!DOCTYPE html>
-
-<body>
-
-</body>
+<html lang="en-GB">
+    <head>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin=""/>
+        <link rel="stylesheet" href="scripts/main.css" />
+        <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
+        <script src="scripts/main.js"></script>
+    </head>
+    <body>
+        <div id="map"></div>
+    </body>
+</html>
