9cca53611fa794b9b2762a8b69d693ea4968fa98
Sean <102238349+sb-shu@users.noreply.github.com>
2023-02-14 10:16:54 +0000

    Brought Leaflet into the repo

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en-GB">
     <head>
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin=""/>
+        <link rel="stylesheet" href="scripts/leaflet.css" />
         <link rel="stylesheet" href="scripts/leaflet.draw.css" />
         <link rel="stylesheet" href="scripts/main.css" />
-        <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
+        <script src="scripts/leaflet.js"></script>
         <script src="scripts/leaflet.draw.js"></script>
         <script src="scripts/main.js"></script>
     </head>
