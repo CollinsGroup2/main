f5c5149b8bb5bace6941eb6a258b483d15daf9d8
Sean <102238349+sb-shu@users.noreply.github.com>
2023-02-16 10:26:21 +0000

    Added chart code

--- scripts/main.js
+++ scripts/main.js
@@ -249,44 +249,6 @@ function popupClose(e) {
     }
 }
 
-// Load the world borders onto Leaflet as a GeoJSON layer
-// Each layer is stored in the borders object.
-function loadBorders() {
-    fetch("assets/border.json")
-        .then((response) => response.json())
-        .then((json) => {
-            const layer = L.geoJson(json, {
-                style: function(feature) {
-                    return {
-                        color: "#0099FF",
-                        fill: false
-                    }
-                },
-                onEachFeature: function(feature, layer) {
-                    const key = feature.properties.ISO2;
-                    borders[key] = layer;
-                }
-            });
-            layer.addTo(map);
-        });
-}
-
-// Calculate the area of a country
-function getCountryArea(code) {
-    const border = borders[code];
-    const ll = border.getLatLngs();
-    let area = 0;
-    // Countries may be comprised of multiple polygons
-    for (let island of ll) {
-        if (island.length < 2) {
-            island = island[0];
-        }
-
-        area += L.GeometryUtil.geodesicArea(island);
-    }
-    return area;
-}
-
 // Wait until the browser has loaded everything before we start initialising things.
 window.onload = function() {
     initLeaflet();
---
+++ scripts/reports.js
@@ -0,0 +1,252 @@
+let products = {};
+let currentProduct = null;
+const dateFormat = Intl.DateTimeFormat("en-GB", {
+    "dateStyle": "long",
+    "timeStyle": "long"
+});
+const borders = {};
+let chart = null;
+
+let minTime = Number.MAX_SAFE_INTEGER;
+let maxTime = Number.MIN_SAFE_INTEGER;
+
+function fetchProducts(pgId) {
+    let url = "backend/get_products.php?";
+    if (pgId) {
+        url += new URLSearchParams({
+            "page": pgId
+        });
+    }
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+            const missions = json["missions"];
+            if (missions.length === 0) {
+                onProductsLoaded();
+                return;
+            }
+
+            for (let i = 0; i < missions.length; i++) {
+                const mission = missions[i];
+                const id = mission[0];
+                products[id] = {
+                    "id": id,
+                    "centre": mission[1],
+                    "creation": mission[2],
+                    "modified": mission[3],
+                    "footprint": mission[4],
+                    "type": mission[5],
+                    "policy": mission[6]
+                };
+
+                minTime = Math.min(minTime, products[id].creation);
+                maxTime = Math.max(maxTime, products[id].creation);
+            }
+
+            if (json["paginationID"]) {
+                getPoints(json["paginationID"]);
+            } else {
+                onProductsLoaded();
+            }
+        });
+}
+
+function updateProductsList() {
+    const list = document.getElementById("products");
+    list.innerHTML = ""; // clear list
+
+    for (const id in products) {
+        const product = products[id];
+        const element = document.createElement("li");
+        const link = document.createElement("a");
+        link.innerText = product.id;
+        link.setAttribute("href", `javascript:selectProduct("${id}");`);
+        element.appendChild(link);
+        list.appendChild(element);
+    }
+}
+
+function onProductsLoaded() {
+    updateProductsList();
+    createChart();
+}
+
+function calculateProductArea(product) {
+    let area = 0;
+    const layer = L.GeoJSON.geometryToLayer(product.footprint);
+    const latlngs = layer.getLatLngs();
+    for (let island of latlngs) {
+        if (island.length < 2) {
+            island = island[0];
+        }
+        area += L.GeometryUtil.geodesicArea(island);
+    }
+    return area;
+}
+
+function selectProduct(id) {
+    if (!products.hasOwnProperty(id)) {
+        return;
+    }
+
+    currentProduct = products[id];
+
+    const ukArea = getCountryArea("GB");
+    let area = calculateProductArea(currentProduct);
+    const ukCoverage = area / ukArea * 100;
+
+    const details = document.getElementById("details");
+    details.innerHTML = `
+        <strong>ID:</strong> ${id}<br/>
+        <strong>Centre:</strong> ${currentProduct.centre}<br/>
+        <strong>Created:</strong> ${dateFormat.format(currentProduct.creation)}<br/>
+        <strong>Modified:</strong> ${dateFormat.format(currentProduct.modified)}<br/>
+        <strong>Type:</strong> ${currentProduct.type}<br/>
+        <strong>Area:</strong> ${L.GeometryUtil.readableArea(area, true, 3)}<br/>
+        <strong>Policy:</strong> ${currentProduct.policy}<br/>
+        <strong>Coverage:</strong> ${ukCoverage.toLocaleString()}%
+    `;
+    createChart(currentProduct.policy);
+}
+
+function calculateGraphDataSets(wantedPolicy) {
+    const ukArea = getCountryArea("GB");
+
+    const sortedProducts = [];
+    for (const id in products) {
+        if (products[id].policy === wantedPolicy) {
+            sortedProducts.push(products[id]);
+        }
+    }
+
+    sortedProducts.sort((a, b) => {
+        if (a.creation < b.creation) return -1;
+        if (a.creation > b.creation) return  1;
+        return 0;
+    });
+
+    let fields = [];
+    let accum = 0;
+    for (const product of sortedProducts) {
+        const creationTime = product.creation;
+
+        const productArea = calculateProductArea(product);
+        const coverage = productArea / ukArea;
+
+        accum += coverage;
+
+        const date = new Date(parseInt(creationTime));
+        fields.push({ x: date, y: accum });
+    }
+
+    return [{
+        label: wantedPolicy,
+        data: fields
+    }];
+}
+
+// Pretty dumb hack to generate a graph for all policies
+// This essentially ensures that each policy holder has a point on the X axis.
+function calculateGraphDataSetsAllPolicies() {
+    const ukArea = getCountryArea("GB");
+
+    let out = [];
+    const sortedProducts = [];
+    for (const id in products) { sortedProducts.push(products[id]); }
+    sortedProducts.sort((a, b) => {
+        if (a.creation < b.creation) return -1;
+        if (a.creation > b.creation) return  1;
+        return 0;
+    });
+
+    const productsByCreation = {};
+    const policies = [];
+    for (const product of sortedProducts) {
+        productsByCreation[product.creation] = product;
+        if (!policies.includes(product.policy)) {
+            policies.push(product.policy);
+        }
+    }
+
+    let data = {};
+    let accum = {};
+    for (const creationTime in productsByCreation) {
+        const product = productsByCreation[creationTime];
+
+        for (const policy of policies) {
+            if (!data.hasOwnProperty(policy)) {
+                data[policy] = [];
+                accum[policy] = 0;
+            }
+
+            if (product.policy === policy) {
+                const productArea = calculateProductArea(product);
+                const coverage = productArea / ukArea;
+                accum[policy] += coverage;
+            }
+
+            const date = new Date(parseInt(creationTime));
+            data[policy].push({ x: date, y: accum[policy] })
+        }
+    }
+
+    for (const policy in data) {
+        out.push({
+            label: policy,
+            data: data[policy]
+        });
+    }
+
+    return out;
+}
+
+function createChart(policy) {
+    const element = document.getElementById("chart");
+    const data = policy ? calculateGraphDataSets(policy) : calculateGraphDataSetsAllPolicies();
+    let title = "Total coverage of the UK";
+    if (policy) {
+        title = `Total coverage of the UK by policy holder ${policy}`
+    }
+
+    if (chart) {
+        chart.destroy();
+        chart = null;
+    }
+
+    chart = new Chart(element, {
+        type: "line",
+        data: {
+            datasets: data
+        },
+        options: {
+            scales: {
+                x: {
+                    type: "time"
+                },
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        min: 0,
+                        callback: (value) => { const pct = value * 100; return pct.toFixed(1) + "%"; }
+                    },
+                    title: {
+                        display: true,
+                        text: "Coverage of UK (%)"
+                    }
+                }
+            },
+            plugins: {
+                title: {
+                    display: true,
+                    text: title
+                }
+            }
+        }
+    });
+}
+
+window.onload = function() {
+    loadBorders();
+    fetchProducts();
+}
